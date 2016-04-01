1cff4e8e8be62501fd0990b7d4f44cf47eafc5fc
Teemu Tiilikainen <teemu.tiilikainen@valuemotive.com>
2016-04-01 21:59:25 +0300

    Add example of basic unit testing

---
+++ tests/ReactNativeTestingUtils.spec.js
@@ -0,0 +1,46 @@
+'use strict';
+
+import './utils';
+import {
+  capitalizeWord,
+  capitalizeWords
+} from '../src/ReactNativeTestingUtils';
+
+
+describe('ReactNativeTestingUtils', function () {
+  describe('capitalizeWord', function () {
+    const words = [
+      'car',
+      'dogsAREbest',
+      'lOreal'
+    ];
+    const expectedWords = [
+      'Car',
+      'Dogsarebest',
+      'Loreal'
+    ];
+    it('should capitalize word', function () {
+      words.forEach((word, index) => {
+        expect(capitalizeWord(word)).to.eql(expectedWords[index]);
+      });
+    });
+  });
+
+  describe('capitalizeWords', function () {
+    const sentences = [
+      'a dOg was   running',
+      'fLyers will win STANLEY CUP',
+      'my AVOCAdo dieD'
+    ];
+    const expectedSentences = [
+      'A Dog Was   Running',
+      'Flyers Will Win Stanley Cup',
+      'My Avocado Died'
+    ];
+    it('should capitalize each word in a sentence', function () {
+      sentences.forEach((sentence, index) => {
+        expect(capitalizeWords(sentence)).to.eql(expectedSentences[index]);
+      });
+    });
+  });
+});
