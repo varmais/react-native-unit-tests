2ec0ed5c8564250281ded1bac54fdd323a103d71
Teemu Tiilikainen <teemu.tiilikainen@valuemotive.com>
2016-07-29 13:49:32 +0300

    Replace mocha-image-compiler with self made require hook

--- mocha-setup.js
+++ mocha-setup.js
@@ -1,3 +1,16 @@
+require('react-native-mock/mock.js');
+
+const m = require('module');
+const originalLoader = m._load;
+
+m._load = function hookedLoader(request, parent, isMain) {
+  if (request.match(/.jpeg|.jpg|.png$/)) {
+    return { uri: request };
+  }
+
+  return originalLoader(request, parent, isMain);
+};
+
 require("babel-register")({
   ignore: /node_modules\/(?!react-native-share)/
 });
--- mocha.runner.sh
+++ mocha.runner.sh
@@ -1,2 +1,2 @@
 #!/usr/bin/env bash
-DEBUG=*$1* BABEL_ENV=test mocha --require react-native-mock/mock.js --compilers png:mocha-image-compiler,js:babel-core/register --watch `find src -name "$1.spec.js" | xargs`
+DEBUG=*$1* BABEL_ENV=test mocha --require mocha-setup.js --compilers js:babel-core/register --watch `find src -name "$1.spec.js" | xargs`
